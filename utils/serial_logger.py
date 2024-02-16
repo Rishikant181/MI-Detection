1f3c6cd9b510c8b90c802ad84f72a6b084207dc2
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-02-16 13:28:57 +0530

    Added simple method to log serial data to file

--- utils/serial_logger.py
+++ utils/serial_logger.py
@@ -1,7 +1,25 @@
-def log(data):
+from serial import Serial
+
+def log(filename):
     '''
     Logs the given data to a file.
 
     Args:
         - data (string): The data to be logged
     '''
+    # Initializing serial connection to COM3
+    serial = Serial('com3')
+
+    # Initializing file
+    file = open(filename, 'w')
+
+    for i in range(10):
+        # Reading the current line from serial
+        data = serial.readline().decode('utf-8').strip('\n')
+
+        # Writing the line
+        file.write(data)
+
+    file.close()
+
+log('test_data.csv')
