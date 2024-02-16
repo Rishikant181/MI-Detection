2bdd8379e3b10ae699dfc050d16b33f5486c1bb1
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-02-16 12:57:01 +0530

    Added ino script for mocking serial data

---
+++ utils/serial_logger.py
@@ -0,0 +1,7 @@
+def log(data):
+    '''
+    Logs the given data to a file.
+
+    Args:
+        - data (string): The data to be logged
+    '''
---
+++ utils/serial_mocker/serial_mocker.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+    // Setting baud rate for serial communications
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // Reading the analog signal at pin A0
+    int value = analogRead(A0);
+
+    // Outputting the value to serial
+    Serial.println(value);
+
+    delay(1000);
+}
