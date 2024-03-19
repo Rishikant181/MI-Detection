54edb8377b58625d081e290b715ebb2764861b66
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-03-19 19:59:28 +0530

    Added functionality to select a single lead while loading data

--- data/prep.py
+++ data/prep.py
@@ -4,16 +4,10 @@ import wfdb
 import ast
 
 def load_raw_annotations(path):
-    Y = pandas.read_csv(path+'ptbxl_database.csv', index_col='ecg_id')
+    Y = pandas.read_csv(path + 'ptbxl_database.csv', index_col = 'ecg_id')
     Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
 
     return Y
-
-def load_raw_data(path, df):
-    data = [wfdb.rdsamp(path+f) for f in df.filename_hr]    
-    data = numpy.array([signal for signal, meta in data])
-
-    return data
 
 def aggregate_diagnostic(y_dic):
     tmp = []
@@ -22,13 +16,34 @@ def aggregate_diagnostic(y_dic):
             tmp.append(agg_df.loc[key].diagnostic_class)
     return list(set(tmp))
 
-path = 'path/to/ptbxl/'
+def load_raw_data(path, df, lead = 0):
+    # If lead == 0, this means all leads are required (default behaviour)
+    if (lead == 0):
+        X = [wfdb.rdsamp(path+f) for f in df.filename_hr]    
+        X = numpy.array([signal for signal, meta in X])
+
+        return X
+    # If lead > 12, this means wrong lead selected, throw error
+    elif (lead < 0 or lead > 12):
+        print("Invalid lead!")
+
+        return None
+    # Else, return the data from the selected lead
+    else:
+        X = numpy.zeros([5000, df.filename_hr.size])
+        for i in range(df.filename_hr.size):
+            signal = numpy.array(wfdb.rdsamp(path + df.filename_hr[i + 1])[0])[:, lead - 1]
+            X[:, i] = signal
+
+        return X
+
+path = '/home/rishikant/Documents/Datasets/PTB-XL/'
 
 # Loading and converting raw annotations data
 Y = load_raw_annotations(path)
 
 # Loading raw signal data
-X = load_raw_data(path, Y)
+X = load_raw_data(path, Y, 1)
 
 # Loading scp_statements.csv for diagnostic aggregation
 agg_df = pandas.read_csv(path+'scp_statements.csv', index_col=0)
